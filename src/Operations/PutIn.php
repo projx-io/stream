513a47f409e47aeea2ad601bbf67aa78de537879
Terrence Howard <chemisus@gmail.com>
2016-02-21 18:34:00 -0500

    testing bug

--- src/Operations/PutIn.php
+++ src/Operations/PutIn.php
@@ -6,9 +6,9 @@ class PutIn
 {
     public function __invoke(&$array, callable $rename, $value, $key)
     {
-        $array = array_merge($array, [call_user_func($rename, $value, $key) => $value]);
+//        $array = array_merge($array, [call_user_func($rename, $value, $key) => $value]);
         // following line of code bugs out for some reason.
-//        $array[call_user_func($rename, $value, $key)] = $value;
+        $array[call_user_func($rename, $value, $key)] = $value;
 
         return $value;
     }
